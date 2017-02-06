0cd6184fb4751d1de47a54a543c569557fda9655
Mike Wang <q13975@gmail.com>
2017-02-06 11:23:53 -0800

    tidy up source code by vi

--- smartapps/q13975/master-and-slave-switches.src/master-and-slave-switches.groovy
+++ smartapps/q13975/master-and-slave-switches.src/master-and-slave-switches.groovy
@@ -14,14 +14,14 @@
  *
  */
 definition(
-    name: "Master and Slave Switches",
-    namespace: "q13975",
-    author: "Mike Wang",
-    description: "A master switch can turn on and off all slave switches. When all slave switches are on, master will be on. When any or all slave switches are off, the master switch will be off accordingly.",
-    category: "Convenience",
-    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
-    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
-    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+	name: "Master and Slave Switches",
+	namespace: "q13975",
+	author: "Mike Wang",
+	description: "A master switch can turn on and off all slave switches. When all slave switches are on, master will be on. When any or all slave switches are off, the master switch will be off accordingly.",
+	category: "Convenience",
+	iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+	iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+	iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
 
 def appVersion() { "1.0.0" }
 def appVerDate() { "2-6-2017" }
@@ -30,12 +30,12 @@ preferences {
 	section("Define a master switch") {
 		input name: "master", type: "capability.switch", title: "Master Switch?", required: true
 	}
-    section("Define slave switches") {
-    	input name: "slaves", type: "capability.switch", title: "Slave Switches?", multiple: true, required: true
-    }
-    section("Master switch will be off if any slaves are off") {
-    	input name: "masterOffAtAll", type: "bool", title: "or until all slaves are off if checked", defaultValue: false
-    }
+	section("Define slave switches") {
+		input name: "slaves", type: "capability.switch", title: "Slave Switches?", multiple: true, required: true
+	}
+	section("Master switch will be off if any slaves are off") {
+		input name: "masterOffAtAll", type: "bool", title: "or until all slaves are off if checked", defaultValue: false
+	}
 }
 
 def installed() {
@@ -63,56 +63,57 @@ def initialize() {
 def handlerMasterOn(evt) {
 	if(state.slaveTriggerOn) {
 	   	state.slaveTriggerOn = false
-    } else {
-    	def offSwitches = getSwitchesByState(slaves, "off")
-        offSwitches?.each { it ->
-        	it.on()
-        }
-    }
+	} else {
+		def offSwitches = getSwitchesByState(slaves, "off")
+		offSwitches?.each { it ->
+			it.on()
+		}
+	}
 }
 
 // Handler when master switch is turned off
 def handlerMasterOff(evt) {
 	if(state.slaveTriggerOff) {
 	   	state.slaveTriggerOff = false
-    } else {
-    	def onSwitches = getSwitchesByState(slaves, "on")
-        onSwitches?.each { it ->
-           	it.off()
-        }
-    }
+	} else {
+		def onSwitches = getSwitchesByState(slaves, "on")
+		onSwitches?.each { it ->
+			it.off()
+		}
+	}
 }
 
 // Handler when slave switch is turned on
 def handlerSlavesOn(evt) {
-    def onSwitches = getSwitchesByState(slaves, "on")
-    if(onSwitches?.size() == slaves.size() && master.currentSwitch == "off") {
-    	state.slaveTriggerOn = true
+	def onSwitches = getSwitchesByState(slaves, "on")
+	if(onSwitches?.size() == slaves.size() && master.currentSwitch == "off") {
+		state.slaveTriggerOn = true
 		master.on()
-    }
+	}
 }
 
 // Handler when slave switch is turned off
 def handlerSlavesOff(evt) {
    	def masterOff = true
 	if(masterOffAtAll) {
-    	def offSwitches = getSwitchesByState(slaves, "off")
-        if(!(offSwitches?.size() == slaves.size())) {
-        	masterOff = false
-        }
+    		def offSwitches = getSwitchesByState(slaves, "off")
+		if(!(offSwitches?.size() == slaves.size())) {
+			masterOff = false
+		}
 	}
-    if(masterOff && master.currentSwitch == "on") {
+	if(masterOff && master.currentSwitch == "on") {
 		state.slaveTriggerOff = true
-	    master.off()
-    }
+		master.off()
+	}
 }
 
 // get a subset from a List of swithes by its state
 private getSwitchesByState(switches, st="on") {
 	if(switches) {
 		st = st?.toLowerCase() == "on" ? "on" : "off"
-    	def result = switches.findAll { it ->
-	    	it.currentSwitch == st ? true : false
-    	}
-    }
+		def result = switches.findAll { it ->
+			it.currentSwitch == st ? true : false
+		}
+	}
 }
+
